ca847abc65cd03a60cc38e7d5b96cb12be5bb178
WieWierz <62114839+WieWierz@users.noreply.github.com>
2021-04-14 10:03:45 +0200

    Add files via upload

---
+++ main10.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include "sha1.c"
+#include "base64.c"
+
+using namespace std;
+
+int main()
+{
+	ifstream plik1;
+	string linia;
+	
+	plik1.open("test3.txt");
+		for(int i;i<6;i++){
+			getline(plik1,linia);
+		}
+	plik1.close();
+	
+	char* char_arr;
+    string str_obj(linia);
+    char_arr = &str_obj[0];
+    cout << char_arr;
+	
+	 	char *magicTest = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"; //36 znakow, GUID Magic string
+        char shaSource[128] = char_arr; //24
+        strcat(shaSource, magicTest); //laczymy te stringi w jeden do zakodowania
+        printf("String kodowany: [%s]\n", shaSource);
+        char result[32];//21
+        SHA1(result, shaSource, strlen(shaSource) ); //Kodowanie SHA1
+        char base64string[128];
+        Base64encode(base64string, result, strlen(result)); //konwersja Base64
+        printf("Po zakodowaniu to [%s] - dlugosc %i bajtow", base64string, strlen(base64string)); //wynikowy string, który wyœlemy w odpowiedzi HandShake.
+        return 0;
+ }
